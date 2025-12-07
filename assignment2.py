07891576941367b685c243eba28237c7da86dbf9
vjena4580-design <vjena4580@gmail.com>
2025-12-07 17:36:20 +0530

    assignment2 added

---
+++ assignment2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Taking user inputs
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating full name
+full_name = first_name + " " + last_name
+
+# Printing personalized greeting
+print(f"\nHello, {full_name}! Welcome to the Python program.")
